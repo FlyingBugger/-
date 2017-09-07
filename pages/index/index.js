510adc46815a4aa39c406c33c87565fefc70daa9
Amani4396 <694767812@qq.com>
2017-09-07 18:02:23 +0800

    9.7

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,33 @@ var app = getApp()
 Page({
   data: {
     motto: 'Hello World',
-    userInfo: {}
+    userInfo: {},
+    show:false,
+    shops:[
+      { 
+        id:1,
+        pic:"../../images/pic.jpg",
+        title:"【包邮】汐美无花果酵素原液30ml*6瓶/盒",
+        counts:"298.00"
+      },
+      {
+        id:2,
+        pic: "../../images/pic.jpg",
+        title: "【包邮】汐美无花果酵素原液30ml*6瓶/盒",
+        counts: "298.00"
+      }
+    ],
+    "search": [
+      {
+        name: "湄公河行动"
+      },
+      {
+        name: "阿甘正传"
+      },
+      {
+        name: "这个杀手不太冷"
+      }
+    ] 
   },
   //事件处理函数
   bindViewTap: function() {
@@ -12,8 +38,10 @@ Page({
       url: '../logs/logs'
     })
   },
+  goDetail:function(e){
+    console.log(e.currentTarget.dataset.id);
+  },
   onLoad: function () {
-    console.log('onLoad')
     var that = this
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
